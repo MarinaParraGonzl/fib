75498dc5f816aa08533d8a8a48663e7778f57f7e
Jose <jacbj42@gmail.com>
2024-11-12 19:24:26 +0100

    feat: add test

--- test_fib.py
+++ test_fib.py
@@ -1,13 +1,16 @@
 from fib import fibonacci_iterative
+from fib import fibonacci_recursive_memoization
 import pytest
 
 
 def test_fib_9_is_34():
     assert fibonacci_iterative(9)==34
-    pass
 
 
 def test_fib_negative_raise_error():
     with pytest.raises(ValueError):
         fibonacci_iterative(-1)
 
+
+def test_fibonacci_recursive_memoization():
+    assert fibonacci_recursive_memoization(9)==34
